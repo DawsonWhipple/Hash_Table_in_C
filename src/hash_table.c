166ca9a7c74b8ab9b3f6c2b220ea7f7e86c2a3f5
DawsonWhippleWSU <dlwhip@outlook.com>
2024-07-10 17:22:14 +0200

    Added Hash Table Functions: Insert, Search, Delete

--- src/hash_table.c
+++ src/hash_table.c
@@ -22,6 +22,8 @@
 #define HT_PRIME_1 2423
 #define HT_PRIME_2 2287
 
+static ht_item HT_DELETED_ITEM = {NULL, NULL};
+
 //initialization of a new ht_item, allocates memory space, and assigns key and value variables, returns a pointer to the new item
 static ht_item* ht_new_item(const char* k, const char* v){
     ht_item *i = malloc(sizeof(ht_item)); //static allocation
@@ -44,12 +46,14 @@ ht_hash_table* ht_new(){
     */
    return ht;
 }
+
 //delete an item from memory and therefore from the hashtable
 static void ht_delete_item(ht_item* i){
     free(i->key);
     free(i->value);
     free(i);
 }
+
 //deletes an entire hash table by individually deleting each item
 void ht_delete_hash_table(ht_hash_table* ht){
     //individually delete all items in the hash table
@@ -61,6 +65,7 @@ void ht_delete_hash_table(ht_hash_table* ht){
     free(ht->items);
     free(ht);
 }
+
 //takes a string as input, returns a number between 0 and m (desired bucket length)
 //The variable a should be a prime number larger than the size of the alphabet.
 //We're hashing ASCII strings, which has an alphabet size of 128, so we should choose a prime larger than that.
@@ -73,9 +78,73 @@ static int ht_hash(const char* s, const int a, const int m){
     }
     return (int) hash;
 }
+
 //handles collisions by running the hash value through multiple different hashing functions
 static int ht_get_hash(const char* s, const int num_buckets, const int attempt){
     const int hash_a = ht_hash(s, HT_PRIME_1, num_buckets);
     const int hash_b = ht_hash(s, HT_PRIME_2, num_buckets);
     return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+//To insert a new key-value pair, we iterate through indexes until we find an empty bucket. 
+//We then insert the item into that bucket and increment the hash table's count attribute, to indicate a new item has been added.
+void ht_insert(ht_hash_table* ht, const char* key, const char* value){
+    ht_item* item = ht_new_item(key, value); //create a blank new item
+    int index = ht_get_hash(item->key, ht->size, 0); //create a starting index hash 
+    ht_item* cur_item = ht->items[index]; //establish the starting item from the starting index
+    int i = 1;
+    //search through indexes until an empty one is found
+    while (cur_item != NULL && cur_item != &HT_DELETED_ITEM) {
+        if (strcmp(cur_item->key, key) == 0) {
+            ht_del_item(cur_item);
+            ht->items[index] = item;
+            return;
+        }
+        index = ht_get_hash(item->key, ht->size, i);
+        cur_item = ht->items[index];
+        i++;
+    } 
+    //add new item to the hash table once an index has been found
+    ht->items[index] = item;
+    ht->count++; //increment counter for amount of entries in the hash table
+}
+
+//Searching is similar to inserting, but at each iteration of the while loop, we check whether the item's key matches the key we're searching for. 
+//If it does, we return the item's value. If the while loop hits a NULL bucket, we return NULL, to indicate that no value was found.
+char* ht_search(ht_hash_table* ht, const char* key){
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (item != &HT_DELETED_ITEM) { 
+            //check if the current key matches the search key
+            if (strcmp(item->key, key) == 0) {
+                return item->value;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    } 
+    return NULL;
+}
+
+//The item we wish to delete may be part of a collision chain. Removing it from the table will break that chain, and will make finding items in the tail of the chain impossible. To solve this, instead of deleting the item, we simply mark it as deleted.
+//We mark an item as deleted by replacing it with a pointer to a global sentinel item which represents that a bucket contains a deleted item.
+void ht_delete(ht_hash_table* ht, const char* key){
+    int index = ht_get_hash(key, ht->size, 0);
+    ht_item* item = ht->items[index];
+    int i = 1;
+    while (item != NULL) {
+        if (item != &HT_DELETED_ITEM) {
+            if (strcmp(item->key, key) == 0) {
+                ht_del_item(item);
+                ht->items[index] = &HT_DELETED_ITEM;
+            }
+        }
+        index = ht_get_hash(key, ht->size, i);
+        item = ht->items[index];
+        i++;
+    } 
+    ht->count--; 
 }
